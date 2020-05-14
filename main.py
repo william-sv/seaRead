c87e0e13b1ea6179958b576d7603b447e1b96739
william <william.sv@icloud.com>
2020-05-14 23:07:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,24 +13,28 @@ import threading
 from spider.xbiquege import Spider as xbiquege_spider
 from spider.dingdianxs import Spider as dingdianxs_sipder
 
-result = []
+class Fiction:
+    def __init__(self):
+        self.result = []
 
+    def search(self,keyword,tag):
+        if tag == 'xbiquege':
+            self.result.append(xbiquege_spider().search(keyword=keyword))
+        if tag == 'dingdianxs':
+            self.result.append(dingdianxs_sipder().search(keyword=keyword))
 
-def search(keyword,tag):
-    if tag == 'xbiquege':
-        result.append(xbiquege_spider().search(keyword=keyword))
-    if tag == 'dingdianxs':
-        result.append(dingdianxs_sipder().search(keyword=keyword))
+    def run(self, keyword):
+        thread1 = threading.Thread(target=self.search, args=(keyword, 'xbiquege'))
+        thread2 = threading.Thread(target=self.search, args=(keyword, 'dingdianxs'))
+        thread1.start()
+        thread2.start()
+        threads = [thread1, thread2]
+        for t in threads:
+            t.join()
+        return self.result
 
 
 if __name__ == '__main__':
     keyword = '烂柯棋缘'
-    thread1 = threading.Thread(target=search, args=(keyword,'xbiquege'))
-    thread2 = threading.Thread(target=search, args=(keyword,'dingdianxs'))
-    thread1.start()
-    thread2.start()
-    threads = [thread1, thread2]
-    for t in threads:
-        t.join()
+    result = Fiction().run(keyword)
     print(result)
-    et = time.time()
